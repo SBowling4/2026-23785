fc0478c25d9a4c18764e6e4ec6efe1d6b3b95cfd
SBowling4 <bowlingmsam@gmail.com>
2025-09-11 13:19:45 -0500

    Indexer almost finished, need ball blue colors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/IndexerSlot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/IndexerSlot.java
@@ -3,11 +3,18 @@ package org.firstinspires.ftc.teamcode.util;
 public class IndexerSlot {
     private Artifact artifact;
 
-    public IndexerSlot(Artifact aritfact) {
-        this.artifact = aritfact;
+    public IndexerSlot(Artifact artifact) {
+        this.artifact = artifact;
     }
 
     public void setArtifact(Artifact artifact) {
         this.artifact = artifact;
+    }
+
+    public enum IndexerPositons {
+        INTAKE,
+        LEFT,
+        SHOOTER,
+        RIGHT
     }
 }
